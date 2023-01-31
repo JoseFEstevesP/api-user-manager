1e08b63df7b3b7253f14ed2e6be4a827bc8169f5
jose esteves <josefelixestevenpacheco@gmail.com>
2023-01-30 22:11:01 -0400

    feat: :wrench: server configurado y conexión a la base de dato
    server configurado y conexión a la base de dato a mongodb

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import { printHello } from '#Lib/hello.js';
+import httpServer from '#Config/http.js';
+import '#Config/env.js';
+import connectDB from '#Config/db.js';
+console.clear()
+const bootstrap = async () => {
+	try {
+		await connectDB(process.env.MONGODB_URL);
+		httpServer.listen(process.env.PORT, () => {
+			console.log(`server in port ${process.env.PORT}`);
+		});		
+	} catch (error) {
+		console.error('connection fails', error)
+	}
 
-printHello();
+};
+bootstrap();
