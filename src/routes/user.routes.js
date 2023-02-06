f38c9870d2f077cdb14bff6e5bb29c4bf9accb57
jose esteves <josefelixestevenpacheco@gmail.com>
2023-02-06 01:58:50 -0400

    feat: :sparkles: controladores agregados
    se agrego los controladores para las diferentes rutas del usuario
    
    ADD : user-login.controller.js ADD : user-profile.controller.js ADD : user-register.controller.js ADD : user-update-data.controller.js ADD : user-update-email.controller.js ADD : user-update-password.controller.js ADD : user-update-unregister.controller.js MODIFIED : user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,3 +1,10 @@
+import userLoginController from '#Controllers/user-login.controller.js';
+import userProfileController from '#Controllers/user-profile.controller.js';
+import userRegisterController from '#Controllers/user-register.controller.js';
+import userUpdateDataController from '#Controllers/user-update-data.controller.js';
+import userUpdateEmailController from '#Controllers/user-update-email.controller.js';
+import userUpdatePasswordController from '#Controllers/user-update-password.controller.js';
+import userUnregisterController from '#Controllers/user-update-unregister.controller.js';
 import userJWTDTO from '#Dto/user-jwt.dto.js';
 import userLoginDTO from '#Dto/user-login.dto.js';
 import userRegisterDTO from '#Dto/user-register.dto.js';
@@ -7,11 +14,31 @@ import userUpdateEmailDTO from '#Dto/user-update-email.dto.js';
 import userUpdatePasswordDTO from '#Dto/user-update-password.dto.js';
 import { Router } from 'express';
 const userRoutes = Router();
-userRoutes.post('/register', userRegisterDTO);
-userRoutes.post('/login', userLoginDTO);
-userRoutes.get('/profile', userJWTDTO);
-userRoutes.patch('/update-data', userJWTDTO, userUpdateDataDTO);
-userRoutes.patch('/update-email', userJWTDTO, userUpdateEmailDTO);
-userRoutes.patch('/update-password', userJWTDTO, userUpdatePasswordDTO);
-userRoutes.delete('/unregister', userJWTDTO, userUnregisterDTO);
+userRoutes.post('/register', userRegisterDTO, userRegisterController);
+userRoutes.post('/login', userLoginDTO, userLoginController);
+userRoutes.get('/profile', userJWTDTO, userProfileController);
+userRoutes.patch(
+  '/update-data',
+  userJWTDTO,
+  userUpdateDataDTO,
+  userUpdateDataController
+);
+userRoutes.patch(
+  '/update-email',
+  userJWTDTO,
+  userUpdateEmailDTO,
+  userUpdateEmailController
+);
+userRoutes.patch(
+  '/update-password',
+  userJWTDTO,
+  userUpdatePasswordDTO,
+  userUpdatePasswordController
+);
+userRoutes.delete(
+  '/unregister',
+  userJWTDTO,
+  userUnregisterDTO,
+  userUnregisterController
+);
 export default userRoutes;
